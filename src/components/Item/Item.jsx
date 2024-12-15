75dbd3044bc50fc591bb9c22879b1c6708ce7c00
David Ho <kietho.060193>
2024-12-15 16:35:28 +0700

    5. Popular.jsx (UI)

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,14 +1,14 @@
 import "./Item.css";
 
-const Item = ({ props }) => {
+const Item = ({ id, name, image, new_price, old_price }) => {
   return (
     <div className="item">
-      <img src={props.image} alt={props.name} />
-      <p>{props.name}</p>
+      <img src={image} alt={name} />
+      <p>{name}</p>
 
       <div className="item-price">
-        <div className="item-price-new">{props.new_price}</div>
-        <div className="item-price-old">{props.old_price}</div>
+        <div className="item-price-new">${new_price}</div>
+        <div className="item-price-old">${old_price}</div>
       </div>
     </div>
   );

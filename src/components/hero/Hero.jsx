3f2fb635642b8a6b6e385e4c17ee4e92c5bf8020
David Ho <kietho.060193>
2024-12-15 15:31:03 +0700

    3. Hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,34 @@
+import "./Hero.css";
+import hand_icon from "../assets/hand_icon.png";
+import arrow_icon from "../assets/arrow.png";
+import hero_image from "../assets/hero_image.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      {/* Left */}
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div>
+          <div className="hero-hand-icon">
+            <p>new</p>
+            <img src={hand_icon} alt="Hand Icon" />
+          </div>
+          <p>collection</p>
+          <p>for everyone</p>
+        </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="Arrow Icon" />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="hero-right">
+        <img src={hero_image} alt="Hero Image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,11 @@
+import Hero from "../components/hero/Hero";
+
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Shop;

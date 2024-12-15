2a121515504fafe5ae377775fbde0a18ef38817f
David Ho <kietho.060193>
2024-12-15 17:23:36 +0700

    7. NewCollections.jsx (UI)

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from "../components/hero/Hero";
+import NewCollection from "../components/newcollections/NewCollection";
 import Offers from "../components/offers/Offers";
 import Popular from "../components/popular/Popular";
 
@@ -8,6 +9,7 @@ const Shop = () => {
       <Hero />
       <Popular />
       <Offers />
+      <NewCollection />
     </div>
   );
 };
